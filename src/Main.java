6e69f4a92905963b0be054851c2138d18f18e874
JudelRos <usr3851@salesianos-lacuesta.net>
2024-04-02 12:45:36 +0100

    feat: Solitar numero usuario

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hola, mundo!");
+        System.out.println("¿Cual es tu nombre?");
+        String numUsuario = scanner.nextLine();
+        System.out.println(numUsuario);
     }
 }
